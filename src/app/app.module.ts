7332b487e7482dbe9a0612a35f2795570758b5a9
joemust0 <joaofss.1991@gmail.com>
2023-09-28 22:27:03 -0300

    (criação de login)

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MeusComponentesComponent } from './meus-componentes/meus-componentes.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeusComponentesComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/meus-componentes/meus-componentes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meus-componentes',
+  templateUrl: './meus-componentes.component.html',
+  styleUrls: ['./meus-componentes.component.css']
+})
+export class MeusComponentesComponent {
+
+}
