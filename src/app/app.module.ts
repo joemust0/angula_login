4c417941c70f7e7b9f7dfa249397b0166fb57e2b
joemust0 <joaofss.1991@gmail.com>
2023-10-04 20:41:22 -0300

    Atualização das rotas2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MeusComponentesComponent } from './meus-componentes/meus-componentes.component';
-import { MainComponent } from './main/main.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeusComponentesComponent,
-    MainComponent,
     HeaderComponent,
     FooterComponent,
     LoginComponent
